88c00f5454c351fcaeb2ee0d0090b51a7a6a0053
fikri <muhamadfirki64@yahoo.co.id>
2021-06-14 10:16:23 +0700

    add dynamic navbar active

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,20 +1,27 @@
 import Link from 'next/link';
+import { withRouter } from 'next/router';
 
-function Navbar() {
+function Navbar({ router }) {
+    const navs = [
+      { text: 'Home', href: '/'},
+      { text: 'About', href: '/about'},
+      { text: 'Portofolio', href: '/portofolio'},
+      { text: 'Contact', href: '/contact'},
+    ];
+
     return (
         <nav className="navbar">
         <div className="container">
           <Link href="/"><a className="logo">Fikri</a></Link>
 
           <ul className="nav-links">
-            <li><Link href="/"><a className="nav-item active">Home</a></Link></li>
-            <li><Link href="/about"><a className="nav-item">About</a></Link></li>
-            <li><Link href="/portofolio"><a className="nav-item">Portofolio</a></Link></li>
-            <li><Link href="/contact"><a className="nav-item">Contact</a></Link></li>
+            { navs.map(nav => (
+              <li><Link href={nav.href}><a className={`nav-item ${router.pathname == nav.href ? 'active' : ''}`}>{nav.text}</a></Link></li>
+            ))}
           </ul>
         </div>
       </nav>
     )
 }
 
-export default Navbar;
+export default withRouter(Navbar);
